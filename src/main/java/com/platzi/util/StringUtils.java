d8dd55b50ca5cbd3b03c49e82cedbd91dfe7f522
fiorde90 <fiorde_90@hotmail.com>
2024-08-29 21:10:27 -0300

    class 4 assert concept

--- src/main/java/com/platzi/util/StringUtils.java
+++ src/main/java/com/platzi/util/StringUtils.java
@@ -4,7 +4,7 @@ public class StringUtils {
 
     public static String repeat(String text, int count) {
         StringBuilder fullText = new StringBuilder();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i <= count; i++) {
             fullText.append(text);
         }
         return fullText.toString();
--- src/test/java/com/platzi/util/StringUtilsTest.java
+++ src/test/java/com/platzi/util/StringUtilsTest.java
@@ -1,13 +1,21 @@
 package com.platzi.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class StringUtilsTest {
 
     public static void main(String[] args) {
 
         String result1 = StringUtils.repeat("hola",3);
-        if (!"holaholahola".equals(result1)) System.out.println("ERROR1"); // si falla, no tenemos información extra
+        String expected1 = "holaholahola";
+        //assertEquals1(result1, expected1);// assert manual
 
         String result2 = StringUtils.repeat("mundo",2);
-        if (!"mundomundo".equals(result2)) System.out.println("ERROR2");
+        String expected2 = "mundomundo";
+        assertEquals(result2, expected2);
+    }
+
+    private static void assertEquals1(String result1, String expected1) {
+        if (!result1.equals(expected1)) throw new RuntimeException(result1 + " is not equal to " + expected1);
     }
 }
