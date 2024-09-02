52adbc7014ad2e7a6772bd63a7317777d5d4171a
fiorde90 <fiorde_90@hotmail.com>
2024-09-02 10:41:35 -0300

    Class 5 assert with JUnit5

--- src/main/java/com/platzi/util/StringUtils.java
+++ src/main/java/com/platzi/util/StringUtils.java
@@ -4,7 +4,7 @@ public class StringUtils {
 
     public static String repeat(String text, int count) {
         StringBuilder fullText = new StringBuilder();
-        for (int i = 0; i <= count; i++) {
+        for (int i = 0; i <= count; i++) { // i < count para que el test pase
             fullText.append(text);
         }
         return fullText.toString();
--- src/test/java/com/platzi/util/StringUtilsTest.java
+++ src/test/java/com/platzi/util/StringUtilsTest.java
@@ -1,21 +1,15 @@
 package com.platzi.util;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringUtilsTest {
 
-    public static void main(String[] args) {
-
-        String result1 = StringUtils.repeat("hola",3);
-        String expected1 = "holaholahola";
-        //assertEquals1(result1, expected1);// assert manual con exception
-
-        String result2 = StringUtils.repeat("mundo",2);
-        String expected2 = "mundomundo";
-        assertEquals(result2, expected2);
-    }
-
-    private static void assertEquals1(String result1, String expected1) {
-        if (!result1.equals(expected1)) throw new RuntimeException(result1 + " is not equal to " + expected1);
+    @Test
+    void testRepeat() {
+        String expected = "mundomundo";
+        String result = StringUtils.repeat("mundo",2);
+        assertEquals(expected, result);
     }
 }
