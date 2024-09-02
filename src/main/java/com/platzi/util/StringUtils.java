9a6eefe10acf60233c36ad1c72b7f0a4a1326e1c
fiorde90 <fiorde_90@hotmail.com>
2024-09-02 10:52:15 -0300

    class 6 test naming and modularization

--- src/main/java/com/platzi/util/StringUtils.java
+++ src/main/java/com/platzi/util/StringUtils.java
@@ -3,6 +3,7 @@ package com.platzi.util;
 public class StringUtils {
 
     public static String repeat(String text, int count) {
+        if (count < 0) throw new IllegalArgumentException("Negative count not allowed");
         StringBuilder fullText = new StringBuilder();
         for (int i = 0; i <= count; i++) { // i < count para que el test pase
             fullText.append(text);
--- src/test/java/com/platzi/util/StringUtilsTest.java
+++ src/test/java/com/platzi/util/StringUtilsTest.java
@@ -3,13 +3,27 @@ package com.platzi.util;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringUtilsTest {
 
     @Test
-    void testRepeat() {
-        String expected = "mundomundo";
-        String result = StringUtils.repeat("mundo",2);
-        assertEquals(expected, result);
+    void repeat_string_once() {
+        assertEquals("mundo", StringUtils.repeat("mundo", 1));
+    }
+
+    @Test
+    void repeat_string_multiple_times() {
+        assertEquals("mundomundomundo", StringUtils.repeat("mundo", 3));
+    }
+
+    @Test
+    void repeat_string_cero_times() {
+        assertEquals("", StringUtils.repeat("mundo", 0));
+    }
+
+    @Test
+    void repeat_string_negative_times() {
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.repeat("mundo", -1));
     }
 }
