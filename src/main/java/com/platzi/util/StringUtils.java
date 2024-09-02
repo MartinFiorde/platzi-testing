df8f17fc7a5292d3c294c8a47b5c48fc8d2f21a4
fiorde90 <fiorde_90@hotmail.com>
2024-09-02 12:10:42 -0300

    clas 7 password tests

--- src/main/java/com/platzi/util/StringUtils.java
+++ src/main/java/com/platzi/util/StringUtils.java
@@ -5,7 +5,7 @@ public class StringUtils {
     public static String repeat(String text, int count) {
         if (count < 0) throw new IllegalArgumentException("Negative count not allowed");
         StringBuilder fullText = new StringBuilder();
-        for (int i = 0; i <= count; i++) { // i < count para que el test pase
+        for (int i = 0; i < count; i++) {
             fullText.append(text);
         }
         return fullText.toString();
--- src/test/java/com/platzi/util/PasswordUtilTest.java
+++ src/test/java/com/platzi/util/PasswordUtilTest.java
@@ -1,6 +1,38 @@
 package com.platzi.util;
 
+import org.junit.jupiter.api.Test;
 
-public class PasswordUtilTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static com.platzi.util.PasswordUtil.SecurityLevel.*;
 
+class PasswordUtilTest {
+    @Test
+    void weak_when_has_less_than_8_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abc456!"));
+    }
+
+    @Test
+    void weak_when_has_only_numbers() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("12345678"));
+    }
+
+    @Test
+    void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdefgh"));
+    }
+
+    @Test
+    void medium_when_has_only_letters_and_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("1234efgh"));
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcd5678"));
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("ab3456gh"));
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("12cdef78"));
+    }
+
+    @Test
+    void strong_when_has_letters_numbers_and_symbols() {
+        assertEquals(STRONG, PasswordUtil.assessPassword("!bcd5678"));
+        assertEquals(STRONG, PasswordUtil.assessPassword("abc4567!"));
+        assertEquals(STRONG, PasswordUtil.assessPassword("abc!5678"));
+    }
 }
