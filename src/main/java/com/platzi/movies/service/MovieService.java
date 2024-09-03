5be8ff5bd7fb399052740fd2b989bd55c3675185
fiorde90 <fiorde_90@hotmail.com>
2024-09-03 18:28:27 -0300

    class 17 mvc tdd with mockedRepository

--- src/main/java/com/platzi/movies/service/MovieService.java
+++ src/main/java/com/platzi/movies/service/MovieService.java
@@ -1,4 +1,21 @@
 package com.platzi.movies.service;
 
+import com.platzi.movies.data.MovieRepository;
+import com.platzi.movies.model.Movie;
+
+import java.util.Collection;
+
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Collection<Movie> findMoviesByGenre(Movie.Genre genre) {
+        return movieRepository.findAll().stream()
+                .filter(movie -> movie.getGenre() == genre)
+                .toList();
+    }
 }
--- src/test/java/com/platzi/movies/service/MovieServiceTest.java
+++ src/test/java/com/platzi/movies/service/MovieServiceTest.java
@@ -1,11 +1,38 @@
 package com.platzi.movies.service;
 
+import com.platzi.movies.data.MovieRepository;
+import com.platzi.movies.model.Movie;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static com.platzi.movies.model.Movie.Genre.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MovieServiceTest {
+class MovieServiceTest {
+    private MovieRepository mockedRepository = Mockito.mock(MovieRepository.class);
+    private MovieService service;
 
-    private MovieService movieService;
+    @Test
+    void return_movies_by_genre() {
+        Mockito.when(mockedRepository.findAll()).thenReturn(Arrays.asList(
+                new Movie(1, "Dark Knight", 152, ACTION),
+                new Movie(2, "Memento", 113, THRILLER),
+                new Movie(3, "There's Something About Mary", 119, COMEDY),
+                new Movie(4, "Super 8", 112, THRILLER),
+                new Movie(5, "Scream", 111, HORROR),
+                new Movie(6, "Home Alone", 103, COMEDY),
+                new Movie(7, "Matrix", 136, ACTION)
+        ));
+        service = new MovieService(mockedRepository);
 
+        Collection<Movie> movies = service.findMoviesByGenre(COMEDY);
+        List<Integer> movieIds = movies.stream().map(Movie::getId).toList();
+
+        assertEquals(List.of(3, 6), movieIds);
+    }
 }
